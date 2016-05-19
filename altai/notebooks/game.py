1c6bc2e30e97effeaa84b669bce9e269ca1f2806
Francis Tseng <f+accounts@frnsys.com>
2016-05-18 23:56:09 -0400

    fixed catch game and included pre-trained weights

--- altai/notebooks/game.py
+++ altai/notebooks/game.py
@@ -11,43 +11,44 @@ class Game():
         self.shape = shape
         self.height, self.width = shape
         self.last_row = self.height - 1
-        self.n_actions = 3 # left, right, stay
+        self.paddle_padding = 1
+        self.n_actions = 3 # left, stay, right
         self.term = Terminal()
         self.reset()
 
     def reset(self):
-        # black & white only
-        self.grid = np.zeros(self.shape, dtype=np.bool)
+        # reset grid
+        self.grid = np.zeros(self.shape)
 
         # can only move left or right (or stay)
         # so position is only its horizontal position (col)
-        self.pos = np.random.randint(self.width - 1)
-
-        # start in the bottom-left corner
-        self.set_position((self.last_row, self.pos), 1)
+        self.pos = np.random.randint(self.paddle_padding, self.width - 1 - self.paddle_padding)
+        self.set_paddle(1)
 
         # item to catch
         self.target = (0, np.random.randint(self.width - 1))
         self.set_position(self.target, 1)
 
     def move(self, action):
-        # clear previous space
-        self.set_position((self.last_row, self.pos), 0)
+        # clear previous paddle position
+        self.set_paddle(0)
 
         # action is either -1, 0, 1,
         # but comes in as 0, 1, 2, so subtract 1
         action -= 1
-        self.pos = min(max(self.pos + action, 0), self.width - 1)
+        self.pos = min(max(self.pos + action, self.paddle_padding), self.width - 1 - self.paddle_padding)
 
-        # set new position
-        self.set_position((self.last_row, self.pos), 1)
+        # set new paddle position
+        self.set_paddle(1)
+
+    def set_paddle(self, val):
+        for i in range(1 + self.paddle_padding*2):
+            pos = self.pos - self.paddle_padding + i
+            self.set_position((self.last_row, pos), val)
 
     @property
     def state(self):
-        # state only needs to consist of
-        # the target's row & col and the player's col
-        # return np.array([self.target[0], self.target[1], self.pos])
-        return self.grid.reshape((1,-1))
+        return self.grid.reshape((1,-1)).copy()
 
     def set_position(self, pos, val):
         r, c = pos
@@ -56,14 +57,19 @@ class Game():
     def update(self):
         r, c = self.target
 
-        # off the map, it's gone
-        if r == self.last_row:
-            # reward of 1 if collided with player, else -1
-            return 1 if c == self.pos else -1
-
         self.set_position(self.target, 0)
         self.target = (r+1, c)
+
+        # off the map, it's gone
+        if r == self.last_row:
+            # reward of 1 if collided with paddle, else -1
+            if abs(c - self.pos) <= self.paddle_padding:
+                return 1
+            else:
+                return -1
+
         self.set_position(self.target, 1)
+
         return 0
 
     def render(self):
@@ -82,13 +88,13 @@ class Game():
 
 
 class Agent():
-    def __init__(self, env, explore=0.2, discount=0.9, hidden_size=100, memory_limit=500):
+    def __init__(self, env, explore=0.1, discount=0.9, hidden_size=100, memory_limit=5000):
         self.env = env
         model = Sequential()
         model.add(Dense(hidden_size, input_shape=(env.height * env.width,), activation='relu'))
         model.add(Dense(hidden_size, activation='relu'))
         model.add(Dense(env.n_actions))
-        model.compile(loss='mse', optimizer='rmsprop')
+        model.compile(loss='mse', optimizer='sgd')
         self.Q = model
 
         # experience replay:
@@ -114,9 +120,7 @@ class Agent():
             Warning('batch size should not be larger than max memory size. Setting batch size to memory size')
             batch_size = self.memory.maxlen
 
-        # only start replaying once we have enough memories
-        if len(self.memory) < batch_size:
-            return
+        batch_size = min(batch_size, len(self.memory))
 
         inputs = []
         targets = []
@@ -124,9 +128,13 @@ class Agent():
         # prep the batch
         # inputs are states, outputs are values over actions
         batch = random.sample(list(self.memory), batch_size)
+        random.shuffle(batch)
         for state, action, next_state, reward in batch:
             inputs.append(state)
             target = self.Q.predict(state)[0]
+
+            # debug, "this should never happen"
+            assert not np.array_equal(state, next_state)
 
             # non-zero reward indicates terminal state
             if reward:
@@ -140,11 +148,8 @@ class Agent():
         # to numpy matrices
         return np.vstack(inputs), np.vstack(targets)
 
-    def replay(self, batch_size=10):
-        batch = self._prep_batch(batch_size)
-        if batch is None:
-            return 0
-        inputs, targets = batch
+    def replay(self, batch_size):
+        inputs, targets = self._prep_batch(batch_size)
         loss = self.Q.train_on_batch(inputs, targets)
         return loss
 
@@ -161,41 +166,46 @@ if __name__ == '__main__':
     from time import sleep
     game = Game()
     agent = Agent(game)
-
-    wins = 0
-    fname = 'game_weights.h5'
+    fname = '../data/game_weights.h5'
 
     if os.path.isfile(fname):
         agent.load(fname)
     else:
         print('training...')
-        epochs = 1000
-        batch_size = 50
+        epochs = 10000
+        batch_size = 256
+
+        # keep track of past record_len results
+        record_len = 100
+        record = deque([], record_len)
 
         for i in range(epochs):
             game.reset()
             reward = 0
             loss = 0
-            state = game.state
             # rewards only given at end of game
             while reward == 0:
+                prev_state = game.state
                 action = agent.choose_action()
                 game.move(action)
                 reward = game.update()
                 new_state = game.state
 
-                agent.remember(state, action, new_state, reward)
+                # debug, "this should never happen"
+                assert not np.array_equal(new_state, prev_state)
+
+                agent.remember(prev_state, action, new_state, reward)
                 loss += agent.replay(batch_size)
-                state = new_state
 
-            sys.stdout.write('epoch: {}/{} | loss: {:.3f} | win rate: {:.3f}\r'.format(i+1, epochs, loss, wins/(i+1)))
             sys.stdout.flush()
+            sys.stdout.write('epoch: {:04d}/{} | loss: {:.3f} | win rate: {:.3f}\r'.format(i+1, epochs, loss, sum(record)/len(record) if record else 0))
 
-            wins += reward if reward == 1 else 0
+            record.append(reward if reward == 1 else 0)
 
-        agent.save('game_weights.h5')
+        agent.save(fname)
 
     game.reset()
+    reward = 0
     while reward == 0:
         game.render()
         action = agent.choose_action()
